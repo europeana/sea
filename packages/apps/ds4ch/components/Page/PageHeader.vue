afd0bdda252295e33e012f1fe1fd4906f1fa3b7b
Leonie Peters <leoniepeters@gmail.com>
2025-06-02 21:02:29 +0200

    refactor: move navbar into its own component

--- packages/apps/ds4ch/components/Page/PageHeader.vue
+++ packages/apps/ds4ch/components/Page/PageHeader.vue
@@ -1,35 +1,5 @@
-<script setup>
-  const logoSrc = '/img/logo.svg';
-</script>
-
 <template>
-  <header id="header" role="banner">
-    <nav role="navigation" class="navbar navbar-expand-lg fixed-top">
-      <NuxtLink to="/" class="logo d-inline-flex">
-        <img :src="logoSrc" alt="Home" data-qa="logo">
-      </NuxtLink>
-      <button
-        class="navbar-toggler" 
-        type="button" 
-        data-bs-toggle="offcanvas" 
-        data-bs-target="#offcanvas-navbar" 
-        aria-controls="offcanvas-navbar" 
-        aria-label="Toggle navigation"
-      >
-          <span class="navbar-toggler-icon"></span>
-      </button>
-      <div 
-        class="offcanvas offcanvas-end"
-        id="offcanvas-navbar"
-        tabindex="-1"
-      >
-          <div class="offcanvas-header">
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" />
-          </div>
-          <div class="offcanvas-body">
-            <PageNavigation />
-          </div>
-      </div>
-    </nav>
+  <header>
+    <PageNavbar />
   </header>
 </template>
