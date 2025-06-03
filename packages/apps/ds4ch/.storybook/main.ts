606edb849ae4f7dd400b1a81ab59379538295f03
Leonie Peters <leoniepeters@gmail.com>
2025-06-03 17:12:50 +0200

    chore: remove storybook onboarding addon

--- packages/apps/ds4ch/.storybook/main.ts
+++ packages/apps/ds4ch/.storybook/main.ts
@@ -7,7 +7,6 @@ const config: StorybookConfig = {
     "../components/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
-    "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@chromatic-com/storybook",
     "@storybook/addon-docs"
