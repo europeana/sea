a88c1efec77ba8bb55656dcab2a6791c4790adc7
Richard Doe <richard.doe@rwdit.net>
2025-06-05 12:59:21 +0100

    style: add prettier and lint-staged (#5)

--- packages/apps/ds4ch/components/Page/PageNavbar.vue
+++ packages/apps/ds4ch/components/Page/PageNavbar.vue
@@ -1,26 +1,37 @@
 <script setup>
-const logoSrc = '/img/logo.svg';
+const logoSrc = "/img/logo.svg";
 
 // TODO: populate URLs when pages exist and translation strings when i18n configured
 const links = [
-  { url: '/', text: 'Home' },
-  { url: '/', text: 'About us' },
-  { url: '/', text: 'Explore the Data space' },
-  { url: '/', text: 'Explore the data' }
-]
+  { url: "/", text: "Home" },
+  { url: "/", text: "About us" },
+  { url: "/", text: "Explore the Data space" },
+  { url: "/", text: "Explore the data" },
+];
 </script>
 <template>
   <nav role="navigation" class="navbar navbar-expand-lg fixed-top">
     <NuxtLink to="/" class="navbar-brand">
-      <img :src="logoSrc" alt="Home">
+      <img :src="logoSrc" alt="Home" />
     </NuxtLink>
-    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvas-navbar"
-      aria-controls="offcanvas-navbar" aria-label="Toggle navigation">
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="offcanvas"
+      data-bs-target="#offcanvas-navbar"
+      aria-controls="offcanvas-navbar"
+      aria-label="Toggle navigation"
+    >
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="offcanvas offcanvas-end" id="offcanvas-navbar" tabindex="-1">
       <div class="offcanvas-header">
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" />
+        <button
+          type="button"
+          class="btn-close"
+          data-bs-dismiss="offcanvas"
+          aria-label="Close"
+        />
       </div>
       <div class="offcanvas-body">
         <ul class="navbar-nav">
