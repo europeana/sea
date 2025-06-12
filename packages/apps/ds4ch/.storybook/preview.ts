128bad66cfe7f664408ab571bd2cf76051538574
Leonie Peters <leoniepeters@gmail.com>
2025-06-12 17:22:30 +0200

    feat: add i18n toolbar to storybook using configuring a locale globalType and decorator

--- packages/apps/ds4ch/.storybook/preview.ts
+++ packages/apps/ds4ch/.storybook/preview.ts
@@ -2,19 +2,55 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 
-import { setup } from "@storybook/vue3-vite";
-import type { Preview } from "@storybook/vue3-vite";
+import type { Preview } from "@nuxtjs/storybook";
+import { watch } from "vue";
 
-// Mock NuxtLink component
-setup((app) => {
-  app.component("NuxtLink", {
-    props: ["to"],
-    template: `<a :href="to"><slot /></a>`,
-  });
-});
+const defaultLocale = "en";
 
-// Automatically generate docs
 const preview: Preview = {
+  globalTypes: {
+    locale: {
+      name: "Locale",
+      description: "Select a language",
+      defaultValue: defaultLocale,
+      toolbar: {
+        icon: "globe",
+        items: [
+          { value: "en", title: "English" },
+          { value: "de", title: "Deutsch" },
+        ],
+      },
+    },
+  },
+  initialGlobals: {
+    locale: defaultLocale,
+  },
+  decorators: [
+    (story, context) => {
+      return {
+        components: { story },
+        template: "<story />",
+        setup() {
+          const i18n = useI18n();
+          watch(
+            () => context.globals.locale,
+            function (newLocale) {
+              return i18n.setLocale(newLocale);
+            },
+            { immediate: true },
+          );
+        },
+      };
+    },
+  ],
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
   tags: ["autodocs"],
 };
 
