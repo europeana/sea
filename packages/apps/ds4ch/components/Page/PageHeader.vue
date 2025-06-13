1d6009444ea0ce9c0fab3e617212b9c960b645d2
Leonie Peters <leoniepeters@gmail.com>
2025-06-13 17:27:29 +0200

    feat: add visible on scroll directive and use in PageHeader

--- packages/apps/ds4ch/components/Page/PageHeader.vue
+++ packages/apps/ds4ch/components/Page/PageHeader.vue
@@ -1,5 +1,18 @@
+<script setup>
+import vVisibleOnScroll from "@europeana/vue-visible-on-scroll";
+</script>
 <template>
-  <header class="bg-dark border-bottom border-light">
+  <header
+    class="bg-dark border-bottom border-light fixed-top"
+    v-visible-on-scroll="$route.hash"
+  >
     <PageNavbar />
   </header>
 </template>
+<style lang="scss" scoped>
+@import "@europeana/style/scss/variables";
+
+header {
+  transition: $standard-transition;
+}
+</style>
---
+++ packages/directives/vue-visible-on-scroll/src/index.js
@@ -0,0 +1,70 @@
+export default {
+  mounted(el, binding) {
+    element = el;
+    routeWithHash = binding.value; // expects $route.hash to be passed
+
+    if (process.browser && onDesktop()) {
+      enableVisibleOnScroll();
+    }
+
+    window.addEventListener("resize", handleResize);
+  },
+
+  // TODO remove even listeners on unmounted? Current use in header, is always mounted
+};
+
+let element;
+let routeWithHash;
+
+let enabled = false;
+const desktopWidth = 992; // Must match $bp-large
+
+const onDesktop = () => window.innerWidth >= desktopWidth;
+
+const handleScroll = () => {
+  const oldPosition = element.scrollPosition ?? 0;
+  const newPosition = window.scrollY;
+  const scrolledDown = oldPosition < newPosition;
+  const scrolledUp = oldPosition - 5 > newPosition;
+
+  if (scrolledDown && oldPosition > 150 && element.scrolledVisible) {
+    element.style.transform = "translate3d(0, -100%, 0)";
+    element.classList.remove("show");
+    element.scrolledVisible = false;
+  } else if ((scrolledUp || oldPosition <= 150) && !element.scrolledVisible) {
+    element.style.transform = "translate3d(0, 0, 0)";
+    element.classList.add("show");
+    element.scrolledVisible = true;
+  }
+  element.scrollPosition = newPosition;
+};
+
+const handleHashChange = () => {
+  element.scrolledVisible = false;
+};
+
+const enableVisibleOnScroll = () => {
+  element.scrolledVisible = !routeWithHash;
+  element.style.transform = "translate3d(0, 0, 0)";
+  element.classList.add("show");
+
+  window.addEventListener("scroll", handleScroll);
+  window.addEventListener("hashchange", handleHashChange);
+  enabled = true;
+};
+
+const disableVisibleOnScroll = () => {
+  window.removeEventListener("scroll", handleScroll);
+  window.removeEventListener("hashchange", handleHashChange);
+  element.style.transform = "translate3d(0, 0, 0)";
+  element.classList.remove("show");
+  enabled = false;
+};
+
+const handleResize = () => {
+  if (!enabled && onDesktop()) {
+    enableVisibleOnScroll();
+  } else if (enabled && !onDesktop()) {
+    disableVisibleOnScroll();
+  }
+};
