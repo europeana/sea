236962bb2a228877321ce614262184f68349b43c
Richard Doe <richard.doe@rwdit.net>
2025-06-23 12:56:49 +0100

    test: run vitest from root

--- packages/apps/ds4ch/vitest.config.ts
+++ packages/apps/ds4ch/vitest.config.ts
@@ -1,12 +1,19 @@
 import { defineVitestConfig } from "@nuxt/test-utils/config";
 import { coverageConfigDefaults } from "vitest/config";
+import { fileURLToPath } from "node:url";
 
 export default defineVitestConfig({
   test: {
-    include: ["**/*.spec.js"],
     coverage: {
       exclude: [...coverageConfigDefaults.exclude, "nuxt.config.ts"],
     },
+    extends: true,
     setupFiles: ["tests/unit.setup.ts"],
+    environment: "nuxt",
+    environmentOptions: {
+      nuxt: {
+        rootDir: fileURLToPath(new URL(".", import.meta.url)),
+      },
+    },
   },
 });
