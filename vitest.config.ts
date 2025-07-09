bc0482c78ea2f71412b3d712ed74048e87c27a80
Leonie Peters <leoniepeters@gmail.com>
2025-07-09 15:38:33 +0200

    test: define files to ignore test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig, coverageConfigDefaults } from "vitest/config";
 
 export default defineConfig({
   test: {
     include: ["**/*.spec.js"],
     projects: ["packages/apps/*"],
+    coverage: {
+      exclude: [
+        ...coverageConfigDefaults.exclude,
+        "**/*.stories.ts",
+        "**/*.config.[jt]s",
+        "packages/apps/ds4ch/i18n/*",
+      ],
+    },
   },
 });
