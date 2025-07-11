89f6048444c20d41e9777135e652b16f16b22902
Richard Doe <richard.doe@rwdit.net>
2025-07-11 14:29:04 +0100

    refactor: use useAsyncData
    to prevent duplicate request on hydration

--- packages/apps/ds4ch/pages/index.vue
+++ packages/apps/ds4ch/pages/index.vue
@@ -1,21 +1,22 @@
 <script setup>
-import { inject } from "vue";
 import landingPageQuery from "@/graphql/queries/landingPage.graphql";
 
 const contentful = inject("$contentful");
 const { localeProperties } = useI18n();
 
-const variables = {
-  identifier: "/",
-  locale: localeProperties.value.language,
-};
+const { data: page } = await useAsyncData("homePage", async () => {
+  const variables = {
+    identifier: "/",
+    locale: localeProperties.value.language,
+  };
 
-const response = await contentful.query(landingPageQuery, variables);
-const { name: title } = response.data?.landingPageCollection?.items?.[0] || {};
+  const response = await contentful.query(landingPageQuery, variables);
+  return response.data?.landingPageCollection?.items?.[0] || {};
+});
 </script>
 
 <template>
   <div style="height: 200px">
-    <h1>{{ title }}</h1>
+    <h1>{{ page.name }}</h1>
   </div>
 </template>
