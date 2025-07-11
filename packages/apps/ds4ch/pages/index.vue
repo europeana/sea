9aa50bd5bcf88ea8a6779410e7774686b6290d8b
Richard Doe <richard.doe@rwdit.net>
2025-07-11 13:06:35 +0100

    feat(ds4ch): get landing page from Contentful

--- packages/apps/ds4ch/pages/index.vue
+++ packages/apps/ds4ch/pages/index.vue
@@ -1,3 +1,21 @@
+<script setup>
+import { inject } from "vue";
+import landingPageQuery from "@/graphql/queries/landingPage.graphql";
+
+const contentful = inject("$contentful");
+const { localeProperties } = useI18n();
+
+const variables = {
+  identifier: "/",
+  locale: localeProperties.value.language,
+};
+
+const response = await contentful.query(landingPageQuery, variables);
+const { name: title } = response.data?.landingPageCollection?.items?.[0] || {};
+</script>
+
 <template>
-  <div class="bg-light" style="height: 200px" />
+  <div class="bg-light" style="height: 200px">
+    <h1>{{ title }}</h1>
+  </div>
 </template>
