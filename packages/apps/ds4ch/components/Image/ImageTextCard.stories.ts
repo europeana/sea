5ff2e361f71a358a8dba27e208898adcd612025d
Leonie Peters <45720401+LeoniePeters@users.noreply.github.com>
2025-07-14 10:24:55 +0200

    feat: image text component (#31)
    * feat: create LandingImageCard component; add refPariry composable and parseMarkdown util
    
    * feat: create ImageTextCard component
    
    ---------
    
    Co-authored-by: lbiedinger <lutz.biedinger@gmail.com>

---
+++ packages/apps/ds4ch/components/Image/ImageTextCard.stories.ts
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from "@nuxtjs/storybook";
+
+import ImageTextCard from "./ImageTextCard.vue";
+import sampleData from "../../.storybook/sample-data.js";
+
+const meta = {
+  component: ImageTextCard,
+} satisfies Meta<typeof ImageTextCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    card: {
+      name: "Card title",
+      text: "This text contains info. It can be __marked__ and accompanied by an image. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: sampleData.imagesWithAttribution[0],
+      link: { text: "read more", url: "/" },
+    },
+  },
+};
+
+export const PrimaryCta: Story = {
+  args: {
+    card: {
+      name: "Card title",
+      text: "This text contains info. It can be __marked__ and accompanied by an image. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: sampleData.imagesWithAttribution[0],
+      link: { text: "read more", url: "/" },
+    },
+    ctaClasses: "btn-primary",
+  },
+};
+
+export const NoCta: Story = {
+  args: {
+    card: {
+      name: "Card title",
+      text: "This text contains info. It can be __marked__ and accompanied by an image. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: sampleData.imagesWithAttribution[0],
+    },
+  },
+};
