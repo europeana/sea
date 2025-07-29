3ab2d825f3d04b3e0546ce45b325577b84dfcd09
Leonie Peters <45720401+LeoniePeters@users.noreply.github.com>
2025-07-29 15:04:05 +0200

    feat: fetch testimonial card group (#35)
    * refactor: rename CardQuoteCard to CardTestimonialCard
    
    * feat: fetch and display testimonial card group on index page
    
    * feat: add card group title, text and styles
    
    * style: wrap testimonial cards to one column on medium breakpoint

--- packages/apps/ds4ch/pages/index.vue
+++ packages/apps/ds4ch/pages/index.vue
@@ -49,6 +49,18 @@ const getClasses = (section) => {
       >
         <ImageTextCard :card="section" />
       </div>
+      <div
+        v-else-if="
+          contentfulEntryHasContentType(section, 'TestimonialCardGroup')
+        "
+        class="container"
+      >
+        <CardGroup
+          :title="section.name"
+          :text="section.text"
+          :cards="section.hasPartCollection.items"
+        />
+      </div>
     </div>
   </div>
 </template>
