2db3c27ff20347fc94979a57d247a4edddc12bd2
Leonie Peters <leoniepeters@gmail.com>
2025-07-31 14:51:10 +0200

    feat: fetch slug pages from ctf

--- packages/apps/ds4ch/pages/[slug].vue
+++ packages/apps/ds4ch/pages/[slug].vue
@@ -1,8 +1,24 @@
 <script setup>
+import landingPageQuery from "@/graphql/queries/landingPage.graphql";
+
 const route = useRoute();
+
+const contentful = inject("$contentful");
+const { localeProperties } = useI18n();
+
+const { data: page } = await useAsyncData("landingPage", async () => {
+  const variables = {
+    identifier: route.params.slug,
+    locale: localeProperties.value.language,
+  };
+
+  const response = await contentful.query(landingPageQuery, variables);
+  return response.data?.landingPageCollection?.items?.[0] || {};
+});
 </script>
 <template>
   <div>
-    <h1>{{ route.fullPath }}</h1>
+    <h1>{{ page.headline }}</h1>
+    {{ page }}
   </div>
 </template>
