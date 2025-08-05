f46dbbde09a5c91169b09134e84b45ca9a0386b2
Leonie Peters <leoniepeters@gmail.com>
2025-08-05 15:27:42 +0200

    fix: set key per slug

--- packages/apps/ds4ch/pages/[slug].vue
+++ packages/apps/ds4ch/pages/[slug].vue
@@ -6,15 +6,18 @@ const route = useRoute();
 const contentful = inject("$contentful");
 const { localeProperties } = useI18n();
 
-const { data: page } = await useAsyncData("landingPage", async () => {
-  const variables = {
-    identifier: route.params.slug,
-    locale: localeProperties.value.language,
-  };
+const { data: page } = await useAsyncData(
+  `landingPage:${route.params.slug}`,
+  async () => {
+    const variables = {
+      identifier: route.params.slug,
+      locale: localeProperties.value.language,
+    };
 
-  const response = await contentful.query(landingPageQuery, variables);
-  return response.data?.landingPageCollection?.items?.[0] || {};
-});
+    const response = await contentful.query(landingPageQuery, variables);
+    return response.data?.landingPageCollection?.items?.[0] || {};
+  },
+);
 
 const sections = page.value.hasPartCollection?.items.filter((item) => !!item);
 </script>
