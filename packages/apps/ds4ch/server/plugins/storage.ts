db17aa2b6593da4e60ba2df77194da0aef0e6ed6
Richard Doe <richard.doe@rwdit.net>
2025-08-11 13:25:29 +0100

    chore: rm console.log

--- packages/apps/ds4ch/server/plugins/storage.ts
+++ packages/apps/ds4ch/server/plugins/storage.ts
@@ -1,10 +1,6 @@
 import redisDriver from "unstorage/drivers/redis";
 
 export default defineNitroPlugin(() => {
-  console.log(
-    "registering nitro plugin for redis storage!",
-    useRuntimeConfig().redis,
-  );
   const storage = useStorage();
 
   // Dynamically pass in credentials from runtime configuration, or other sources
