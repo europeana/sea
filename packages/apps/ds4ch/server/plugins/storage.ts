2f54f5f26e416829f57d00968306bf3419177ce9
Richard Doe <richard.doe@rwdit.net>
2025-08-11 13:26:07 +0100

    chore: rm excess comments

--- packages/apps/ds4ch/server/plugins/storage.ts
+++ packages/apps/ds4ch/server/plugins/storage.ts
@@ -3,12 +3,10 @@ import redisDriver from "unstorage/drivers/redis";
 export default defineNitroPlugin(() => {
   const storage = useStorage();
 
-  // Dynamically pass in credentials from runtime configuration, or other sources
   const driver = redisDriver({
     base: "@europeana:portal.js:",
     url: useRuntimeConfig().redis.url,
   });
 
-  // Mount driver
   storage.mount("redis", driver);
 });
