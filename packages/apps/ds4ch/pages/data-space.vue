f031acaf507bc2b2143efee1108e94767e9b3cc9
Richard Doe <richard.doe@rwdit.net>
2025-08-15 10:36:24 +0100

    feat(ds4ch): handle pagination

--- packages/apps/ds4ch/pages/data-space.vue
+++ packages/apps/ds4ch/pages/data-space.vue
@@ -5,6 +5,11 @@ import blogPostListingPageQuery from "@/graphql/queries/blogPostListingPage.grap
 const slug = "data-space";
 const contentful = inject("$contentful");
 const { localeProperties } = useI18n();
+const route = useRoute();
+
+const page = computed(() => Number(route.query.page) || 1);
+const limit = 24;
+const skip = computed(() => (page.value - 1) * limit);
 
 const { data } = await useAsyncData(`landingPage:${slug}`, async () => {
   const variables = {
@@ -14,7 +19,11 @@ const { data } = await useAsyncData(`landingPage:${slug}`, async () => {
 
   const responses = await Promise.all([
     contentful.query(landingPageQuery, variables),
-    contentful.query(blogPostListingPageQuery, variables),
+    contentful.query(blogPostListingPageQuery, {
+      ...variables,
+      limit,
+      skip: skip.value,
+    }),
   ]);
 
   return {
@@ -23,22 +32,21 @@ const { data } = await useAsyncData(`landingPage:${slug}`, async () => {
   };
 });
 
-const { page, posts } = data.value;
-
 useHead({
-  title: page.headline,
+  title: data.value.page.headline,
 });
 </script>
+
 <template>
   <div>
     <LandingHero
-      :headline="page.headline || ''"
-      :text="page.text"
-      :hero-image="page.primaryImageOfPage"
+      :headline="data.page.headline || ''"
+      :text="data.page.text"
+      :hero-image="data.page.primaryImageOfPage"
       variant="alternate"
     />
-    <ol v-if="posts.length > 0">
-      <li v-for="(post, index) in posts" :key="index">
+    <ol v-if="data.posts.length > 0">
+      <li v-for="(post, index) in data.posts" :key="index">
         <NuxtLinkLocale
           :to="{ name: 'news-slug', params: { slug: post.identifier } }"
         >
