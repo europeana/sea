ae85024907181a7320fd88b893305687012e1248
lbiedinger <lutz.biedinger@gmail.com>
2025-08-18 09:25:26 +0200

    feat(ds4ch): get blog post description for listing

--- packages/apps/ds4ch/pages/data-space.vue
+++ packages/apps/ds4ch/pages/data-space.vue
@@ -50,7 +50,7 @@ useHead({
         <NuxtLinkLocale
           :to="{ name: 'news-slug', params: { slug: post.identifier } }"
         >
-          {{ post.name }}
+          {{ post.name }} - {{ post.description }}
         </NuxtLinkLocale>
       </li>
     </ol>
