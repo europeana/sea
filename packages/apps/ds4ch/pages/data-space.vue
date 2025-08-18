cfc266ebe339149eebe96df9cbcb4293530eb8fe
Richard Doe <richard.doe@rwdit.net>
2025-08-18 08:28:21 +0100

    feat(ds4ch): fetch news posts from Contentful (#51)
    * feat(ds4ch): fetch news posts from Contentful
    
    to display on the data-space page
    
    * feat(ds4ch): handle pagination
    
    * feat(ds4ch): get blog post description for listing
    
    ---------
    
    Co-authored-by: lbiedinger <lutz.biedinger@gmail.com>

--- packages/apps/ds4ch/pages/data-space.vue
+++ packages/apps/ds4ch/pages/data-space.vue
@@ -1,27 +1,58 @@
 <script setup>
 import landingPageQuery from "@/graphql/queries/landingPage.graphql";
+import blogPostListingPageQuery from "@/graphql/queries/blogPostListingPage.graphql";
 
 const slug = "data-space";
 const contentful = inject("$contentful");
 const { localeProperties } = useI18n();
+const route = useRoute();
 
-const { data: page } = await useAsyncData(`landingPage:${slug}`, async () => {
+const page = computed(() => Number(route.query.page) || 1);
+const limit = 24;
+const skip = computed(() => (page.value - 1) * limit);
+
+const { data } = await useAsyncData(`landingPage:${slug}`, async () => {
   const variables = {
     identifier: slug,
     locale: localeProperties.value.language,
   };
 
-  const response = await contentful.query(landingPageQuery, variables);
-  return response.data?.landingPageCollection?.items?.[0] || {};
+  const responses = await Promise.all([
+    contentful.query(landingPageQuery, variables),
+    contentful.query(blogPostListingPageQuery, {
+      ...variables,
+      limit,
+      skip: skip.value,
+    }),
+  ]);
+
+  return {
+    page: responses[0].data?.landingPageCollection?.items?.[0] || {},
+    posts: responses[1].data?.blogPostingCollection?.items || [],
+  };
+});
+
+useHead({
+  title: data.value.page.headline,
 });
 </script>
+
 <template>
   <div>
     <LandingHero
-      :headline="page.headline || ''"
-      :text="page.text"
-      :hero-image="page.primaryImageOfPage"
+      :headline="data.page.headline || ''"
+      :text="data.page.text"
+      :hero-image="data.page.primaryImageOfPage"
       variant="alternate"
     />
+    <ol v-if="data.posts.length > 0">
+      <li v-for="(post, index) in data.posts" :key="index">
+        <NuxtLinkLocale
+          :to="{ name: 'news-slug', params: { slug: post.identifier } }"
+        >
+          {{ post.name }} - {{ post.description }}
+        </NuxtLinkLocale>
+      </li>
+    </ol>
   </div>
 </template>
