e5afb56333fb9438ed805cf6778b8f109154bc32
Richard Doe <richard.doe@rwdit.net>
2025-08-19 10:52:30 +0100

    refactor(ds4ch): mv LandingIllustrationGroup to base
    and add a container to ds4ch

---
+++ packages/apps/ds4ch/components/Landing/LandingIllustrationGroupContainer.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="landing-illustration-group-container">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "assets/scss/variables";
+@import "@europeana/style/scss/responsive-background-image";
+
+.landing-illustration-group-container :deep(.landing-illustration-group) {
+  @media (min-width: $bp-4k) {
+    margin-top: 15rem;
+    margin-bottom: 15rem;
+  }
+
+  .header {
+    @media (min-width: $bp-4k) {
+      max-width: $max-text-column-width-landing-4k !important;
+      padding-bottom: 5rem;
+    }
+  }
+
+  h2 {
+    @media (min-width: $bp-4k) {
+      margin-bottom: 2rem;
+    }
+  }
+
+  .text {
+    color: $black;
+
+    @media (min-width: $bp-4k) {
+      font-size: 2.5rem;
+      margin-bottom: 3.125rem;
+    }
+  }
+
+  $swiper-slide-width-height-ds4ch-4k: 340px;
+  $swiper-pagination-height-ds4ch-4k: 126px;
+
+  .swiper-container-wrapper {
+    @media (min-width: $bp-4k) {
+      height: $swiper-slide-width-height-ds4ch-4k;
+
+      &.paginated {
+        height: calc(
+          $swiper-slide-width-height-ds4ch-4k +
+            $swiper-pagination-height-ds4ch-4k
+        );
+      }
+    }
+  }
+  .swiper-container {
+    .image-wrapper {
+      @media (min-width: $bp-4k) {
+        width: $swiper-slide-width-height-ds4ch-4k;
+        height: $swiper-slide-width-height-ds4ch-4k;
+      }
+    }
+  }
+
+  .swiper-button-prev,
+  .swiper-button-next {
+    @media (min-width: $bp-4k) {
+      height: 128px;
+      width: 128px;
+
+      &:after {
+        font-size: 3.5rem;
+      }
+    }
+  }
+
+  .swiper-pagination {
+    @media (min-width: $bp-4k) {
+      margin-top: 4rem;
+    }
+  }
+
+  :deep(.swiper-pagination-bullet) {
+    border: 2px solid $black;
+
+    @media (min-width: $bp-4k) {
+      height: 32px;
+      width: 32px;
+      border: 4px solid $black;
+      margin-right: 12px;
+
+      &:last-child {
+        margin-right: 0;
+      }
+    }
+
+    &.swiper-pagination-bullet-active {
+      background-color: $black;
+    }
+  }
+}
+</style>
--- packages/apps/ds4ch/components/Page/PageSections.vue
+++ packages/apps/ds4ch/components/Page/PageSections.vue
@@ -38,14 +38,17 @@ const getClasses = (section) => {
     >
       <ImageTextCard :card="section" />
     </div>
-    <LandingIllustrationGroup
+    <LandingIllustrationGroupContainer
       v-else-if="contentfulEntryHasContentType(section, 'IllustrationGroup')"
-      :title="section.name"
-      :text="section.text"
-      :illustrations="
-        section.hasPartCollection && section.hasPartCollection.items
-      "
-    />
+    >
+      <LandingIllustrationGroup
+        :title="section.name"
+        :text="section.text"
+        :illustrations="
+          section.hasPartCollection && section.hasPartCollection.items
+        "
+      />
+    </LandingIllustrationGroupContainer>
     <div
       v-else-if="contentfulEntryHasContentType(section, 'TestimonialCardGroup')"
       class="container testimonial-card-container"
