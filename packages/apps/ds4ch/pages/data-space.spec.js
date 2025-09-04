153de48710f77f40a1fa17ef95e74e1a997cc408
lbiedinger <lutz.biedinger@gmail.com>
2025-09-04 10:10:35 +0200

    feat: use content-interface on data-space page

--- packages/apps/ds4ch/pages/data-space.spec.js
+++ packages/apps/ds4ch/pages/data-space.spec.js
@@ -16,6 +16,18 @@ mockNuxtImport("useRuntimeConfig", () => {
   });
 });
 
+mockNuxtImport("useRoute", () => {
+  return () => {
+    return {
+      path: "/data-space",
+      fullPath: "/data-space",
+      query: {
+        page: 1,
+      },
+    };
+  };
+});
+
 mockNuxtImport("useI18n", () => {
   return () => {
     return {
@@ -29,17 +41,59 @@ mockNuxtImport("useI18n", () => {
 
 const title = "Explore the data space";
 const description = "DS4CH text";
-const defaultContentfulResponse = {
+
+const blogPostingListingMinimalContentfulResponse = {
   data: {
     blogPostingCollection: {
       items: [
         {
-          name: "News post",
-          identifier: "post",
-          primaryImageOfPage: { image: {} },
+          date: "2022-02-12T08:00:00.000+01:00",
+          sys: { id: "796f5YKe4b1u8uXtizSBu0" },
+          cats: { items: [{ id: "3d" }, null] },
+        },
+        {
+          date: "2023-02-12T07:00:00.000+01:00",
+          sys: { id: "2abe0ffb2af8b39dfdb83d" },
+          cats: { items: [{ id: "network" }, null] },
         },
       ],
     },
+  },
+};
+
+const contentBySysIdContentfulResponse = {
+  data: {
+    blogPostingCollection: {
+      items: [
+        {
+          __typename: "BlogPosting",
+          sys: {
+            id: "796f5YKe4b1u8uXtizSBu0",
+          },
+          identifier: "blog-identifier",
+          name: "Blog entry",
+          primaryImageOfPage: {
+            image: {
+              url: "https://images.ctfassets.net/i01duvb6kq77/7Jnq4yka0vfYdWKo7IV7Dc/5778788f0359b1a6a81ef1f57a260982/feature_1920Olympics.jpg",
+              contentType: "image/jpeg",
+            },
+          },
+        },
+        {
+          __typename: "BlogPosting",
+          sys: {
+            id: "2abe0ffb2af8b39dfdb83d",
+          },
+          identifier: "blog-no-image",
+          name: "Blog no Image",
+        },
+      ],
+    },
+  },
+};
+
+const landingPageContentfulResponse = {
+  data: {
     landingPageCollection: {
       items: [
         {
@@ -51,12 +105,45 @@ const defaultContentfulResponse = {
     },
   },
 };
-const factory = async (contentfulResponse = defaultContentfulResponse) =>
+
+const categoriesContentfulResponse = {
+  data: {
+    categoryCollection: {
+      items: [
+        {
+          identifier: "3d",
+          name: "3D",
+        },
+        {
+          identifier: "network",
+          name: "network",
+        },
+      ],
+    },
+  },
+};
+
+const handleContentfulQuery = (graphQL) => {
+  if (graphQL.definitions?.[0]?.name?.value === "ContentBySysId") {
+    return contentBySysIdContentfulResponse;
+  }
+  if (graphQL.definitions?.[0]?.name?.value === "BlogPostingListingMinimal") {
+    return blogPostingListingMinimalContentfulResponse;
+  }
+  if (graphQL.definitions?.[0]?.name?.value === "LandingPage") {
+    return landingPageContentfulResponse;
+  }
+  if (graphQL.definitions?.[0]?.name?.value === "Categories") {
+    return categoriesContentfulResponse;
+  }
+};
+
+const factory = async () =>
   await mountSuspended(dataSpacePage, {
     global: {
       provide: {
         $contentful: {
-          query: () => contentfulResponse,
+          query: (graphQL) => handleContentfulQuery(graphQL),
         },
       },
     },
@@ -83,24 +170,26 @@ describe("dataSpacePage", () => {
   it("renders content card links to the retrieved news posts", async () => {
     const wrapper = await factory();
 
-    const contentCardLink = wrapper.find(".content-card a");
+    const contentCardLinks = wrapper.findAll(".card-link");
 
-    expect(contentCardLink.attributes().href).toBe("/en/news/post");
+    expect(contentCardLinks[0].attributes("href")).toBe(
+      "/en/news/blog-no-image",
+    );
+    expect(contentCardLinks[1].attributes("href")).toBe(
+      "/en/news/blog-identifier",
+    );
   });
 
-  it("sets a default thumbnail for cards without an image", async () => {
-    const contentfulResponseNoImage = { ...defaultContentfulResponse };
-    contentfulResponseNoImage.data.blogPostingCollection.items.forEach(
-      (item) => delete item.primaryImageOfPage,
-    );
+  it("renders images for each news posts", async () => {
+    const wrapper = await factory();
 
-    const wrapper = await factory(contentfulResponseNoImage);
+    const contentCardImages = wrapper.findAll(".card-img img");
 
-    expect(wrapper.vm.cards.every((card) => !!card.primaryImageOfPage)).toBe(
-      true,
+    expect(contentCardImages[0].attributes("src")).toBe(
+      "https://www.example.org/image.jpg",
     );
-    expect(wrapper.vm.cards[0].primaryImageOfPage.image.url).toBe(
-      defaultThumbnail,
+    expect(contentCardImages[1].attributes("src")).toBe(
+      "https://images.ctfassets.net/i01duvb6kq77/7Jnq4yka0vfYdWKo7IV7Dc/5778788f0359b1a6a81ef1f57a260982/feature_1920Olympics.jpg?q=80&fm=webp",
     );
   });
 });
