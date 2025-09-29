b7bb38844dd83f2527d5e1c9d191fa2e5a04fe42
Richard Doe <richard.doe@rwdit.net>
2025-09-29 11:18:52 +0100

    fix(cats): parenthesise field fallback

--- packages/apps/cats/server/utils/html/markupFields.ts
+++ packages/apps/cats/server/utils/html/markupFields.ts
@@ -18,7 +18,7 @@ export const markupFields = ({ entry, contentType, locale = "en-GB" }) => {
       const section = doc("<section></section>");
       section.attr("data-ctf-field-id", field.id);
 
-      if (field.items?.type || field.type === "Text") {
+      if ((field.items?.type || field.type) === "Text") {
         // assume markdown, and convert to html
         section.html(marked.parse(fieldValue));
       } else {
