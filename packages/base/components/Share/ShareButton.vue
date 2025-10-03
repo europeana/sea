11cde3d5b03d590af0779682ad8928b0103dfb04
lbiedinger <lutz.biedinger@gmail.com>
2025-10-03 14:01:53 +0200

    feat: log matomo events for tags & share modals

--- packages/base/components/Share/ShareButton.vue
+++ packages/base/components/Share/ShareButton.vue
@@ -1,10 +1,23 @@
 <script setup>
+const route = useRoute();
+const nuxtApp = useNuxtApp();
+
 defineProps({
   variant: {
     type: String,
     default: "btn-secondary",
   },
 });
+
+const logModalOpen = () => {
+  if (nuxtApp.vueApp?.config?.globalProperties?.$matomo) {
+    nuxtApp.vueApp.config.globalProperties.$matomo.trackEvent(
+      "Open modal",
+      "Social share modal opened",
+      `Opened share modal on ${route.path}`,
+    );
+  }
+};
 </script>
 
 <template>
@@ -13,6 +26,7 @@ defineProps({
     :class="variant"
     data-bs-toggle="modal"
     data-bs-target="#share-modal"
+    @click="logModalOpen"
   >
     <span class="icon-share d-inline-flex me-1 me-4k-3" />
     {{ $t("actions.share") }}
