72827148bc542fff08362d442f6862aa1d765a0d
Leonie Peters <45720401+LeoniePeters@users.noreply.github.com>
2025-10-22 11:23:17 +0200

    fix(base): transform hash destination into route object (#108)
    * fix(base): transform hash destination into route object
    
    * docs(base): add note & todo re NuxtLinkLocale # links
    
    ---------
    
    Co-authored-by: Richard Doe <richard.doe@rwdit.net>

--- packages/base/components/Generic/SmartLink.vue
+++ packages/base/components/Generic/SmartLink.vue
@@ -15,6 +15,7 @@ const props = defineProps({
 });
 
 const internalDomain = useRuntimeConfig().public.internalLinkDomain;
+const route = useRoute();
 
 const isExternalLink = computed(() => {
   const path = props.destination;
@@ -32,10 +33,24 @@ const isExternalLink = computed(() => {
   const hostname = hostnamePattern.exec(path)[1];
   return !hostname.endsWith(internalDomain);
 });
+
+const path = computed(() => {
+  if (
+    typeof props.destination === "string" &&
+    props.destination.startsWith("#")
+  ) {
+    // NOTE: NuxtLinkLocale does not link properly when `to` prop is a string
+    //       starting with '#', i.e. an anchor link.
+    // TODO: stop using NuxtLinkLocale; replicate its functionality here.
+    return { ...route, hash: props.destination };
+  }
+
+  return props.destination;
+});
 </script>
 <template>
   <NuxtLinkLocale
-    :to="props.disabled ? null : destination"
+    :to="props.disabled ? null : path"
     :target="isExternalLink ? '_blank' : null"
     :external="isExternalLink"
   >
