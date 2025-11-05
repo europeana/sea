06cfdab796b8f49447930a34450577b202f10afa
Leonie Peters <45720401+LeoniePeters@users.noreply.github.com>
2025-11-05 13:44:37 +0100

    chore(ds4ch): remove hotjar and comment out third party services (#118)

--- packages/apps/ds4ch/utils/services/services.js
+++ packages/apps/ds4ch/utils/services/services.js
@@ -29,27 +29,26 @@ const parseDefinitions = (purpose) =>
     return service;
   });
 
-const mediaViewingServices = [
-  ...parseDefinitions("2D"),
-  ...parseDefinitions("3D"),
-  ...parseDefinitions("audio"),
-  ...parseDefinitions("video"),
-].map((service) => ({
-  ...service,
-  purposes: ["mediaViewing", ...service.purposes],
-}));
+// const mediaViewingServices = [
+//   ...parseDefinitions("2D"),
+//   ...parseDefinitions("3D"),
+//   ...parseDefinitions("audio"),
+//   ...parseDefinitions("video"),
+// ].map((service) => ({
+//   ...service,
+//   purposes: ["mediaViewing", ...service.purposes],
+// }));
 
-const thirdPartyServices = [
-  ...parseDefinitions("socialMedia"),
-  ...mediaViewingServices,
-].map((service) => ({
-  ...service,
-  purposes: ["thirdPartyContent", ...service.purposes],
-}));
+// const thirdPartyServices = [
+//   ...parseDefinitions("socialMedia"),
+//   ...mediaViewingServices,
+// ].map((service) => ({
+//   ...service,
+//   purposes: ["thirdPartyContent", ...service.purposes],
+// }));
 
-// TODO comment out thirdPartyServices before merge
 // TODO add ...thirdPartyServices when embed gateway is in place
-const services = [...parseDefinitions("ds4ch"), ...thirdPartyServices];
+const services = [...parseDefinitions("ds4ch")];
 
 const essentialServicesNames = services
   .filter((s) => s.required)
