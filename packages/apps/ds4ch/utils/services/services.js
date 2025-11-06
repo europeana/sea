1337f216229b125de85cf33aa0cf295f94164a83
Leonie Peters <leoniepeters@gmail.com>
2025-11-06 17:04:48 +0100

    chore(ds4ch): add more services

--- packages/apps/ds4ch/utils/services/services.js
+++ packages/apps/ds4ch/utils/services/services.js
@@ -3,6 +3,7 @@ import twoD from "./definitions/2D.json";
 import threeD from "./definitions/3D.json";
 import audio from "./definitions/audio.json";
 import ds4ch from "./definitions/ds4ch.json";
+import other from "./definitions/other.json";
 import socialMedia from "./definitions/socialMedia.json";
 import video from "./definitions/video.json";
 import * as callbacks from "./callbacks/index.js";
@@ -12,6 +13,7 @@ const definitions = {
   "3D": threeD,
   audio,
   ds4ch,
+  other,
   socialMedia,
   video,
 };
@@ -42,6 +44,7 @@ const mediaViewingServices = [
 const thirdPartyServices = [
   ...parseDefinitions("socialMedia"),
   ...mediaViewingServices,
+  ...parseDefinitions("other"),
 ].map((service) => ({
   ...service,
   purposes: ["thirdPartyContent", ...service.purposes],
