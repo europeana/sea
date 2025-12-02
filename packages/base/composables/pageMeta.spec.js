0499b5ee9023f2d12980bac7459e3e143d97e8d3
lbiedinger <lutz.biedinger@gmail.com>
2025-12-02 16:21:15 +0100

    test: add specs for pageMeta

---
+++ packages/base/composables/pageMeta.spec.js
@@ -0,0 +1,59 @@
+import { describe, it, expect, vi, afterEach } from "vitest";
+import { mockNuxtImport } from "@nuxt/test-utils/runtime";
+import { usePageMeta } from "./pageMeta";
+
+const { useSeoMetaMock } = vi.hoisted(() => ({
+  useSeoMetaMock: vi.fn(() => ({})),
+}));
+mockNuxtImport("useSeoMeta", () => useSeoMetaMock);
+
+const fixtures = {
+  title: "page title",
+  description: "page seo description",
+  image: { url: "https://example.org/image.jpg", description: "Preview image" },
+};
+const defaultExpectations = {
+  title: fixtures.title,
+  description: fixtures.description,
+  ogTitle: fixtures.title,
+  ogDescription: fixtures.description,
+  ogImage: fixtures.image.url,
+  ogImageAlt: fixtures.image.description,
+  ogType: undefined,
+};
+describe("usePageMeta", () => {
+  afterEach(() => {
+    useSeoMetaMock.mockReset();
+  });
+
+  describe("passing title, description and image", () => {
+    it("uses useSeoMeta to set the html meta tags", () => {
+      usePageMeta(fixtures);
+
+      expect(useSeoMetaMock).toHaveBeenCalledWith(defaultExpectations);
+    });
+  });
+
+  describe("when called without an image", () => {
+    it("does not pass image data to the useSeoMeta call", () => {
+      usePageMeta({ ...fixtures, image: false });
+
+      expect(useSeoMetaMock).toHaveBeenCalledWith({
+        ...defaultExpectations,
+        ogImage: undefined,
+        ogImageAlt: undefined,
+      });
+    });
+  });
+
+  describe("when called with an ogType", () => {
+    it("uses useSeoMeta to set the 'article' ogType", () => {
+      usePageMeta({ ...fixtures, ogType: "article" });
+
+      expect(useSeoMetaMock).toHaveBeenCalledWith({
+        ...defaultExpectations,
+        ogType: "article",
+      });
+    });
+  });
+});
