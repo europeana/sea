3015c9f17284b2cbed47e5c1623e81061cc48b73
Leonie Peters <leoniepeters@gmail.com>
2026-01-15 18:14:47 +0100

    chore(base): update order of links in type filter

--- packages/base/components/Content/ContentTypeFilter.vue
+++ packages/base/components/Content/ContentTypeFilter.vue
@@ -19,11 +19,11 @@ const props = defineProps({
 const availableContentTypes = [{ name: t("content.filter.viewAll") }].concat(
   [
     { name: t("content.filter.news"), query: "news", type: "blog post" },
-    { name: t("content.filter.projects"), query: "project" },
     { name: t("content.filter.stories"), query: "story" },
     { name: t("content.filter.exhibitions"), query: "exhibition" },
     { name: t("content.filter.events"), query: "event" },
     { name: t("content.filter.training"), query: "training" },
+    { name: t("content.filter.projects"), query: "project" },
   ].filter((type) => props.contentTypes.includes(type.type || type.query)),
 );
 
