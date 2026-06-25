1acf33c76cf633c6a929407341abcce93b2673ea
Richard Doe <richard.doe@rwdit.net>
2026-06-25 08:04:03 +0100

    refactor: mv fetching data to component

--- packages/components/map/src/components/EuropeanaMap.spec.js
+++ packages/components/map/src/components/EuropeanaMap.spec.js
@@ -1,15 +1,39 @@
 // @vitest-environment happy-dom
 
 import { shallowMount } from "@vue/test-utils";
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
+import { useFetch } from "@vueuse/core";
 
 import EuropeanaMap from "./EuropeanaMap.vue";
 import { fixtures } from "@test/fixtures.js";
 
+const mocks = vi.hoisted(() => {
+  return {
+    useFetch: vi.fn(),
+  };
+});
+
+vi.mock("@vueuse/core", () => {
+  return {
+    useFetch: mocks.useFetch,
+  };
+});
+
+vi.mocked(useFetch).mockReturnValue({
+  json: vi.fn().mockResolvedValue({
+    data: { value: fixtures.twoPointsFeatureCollection },
+  }),
+});
+
+const factory = ({ props } = {}) =>
+  shallowMount(EuropeanaMap, {
+    props,
+  });
+
 describe("@/components/EuropeanaMap.vue", () => {
   it("renders a map container element", () => {
-    const wrapper = shallowMount(EuropeanaMap, {
-      propsData: {
+    const wrapper = factory({
+      props: {
         json: JSON.stringify(fixtures.onePointFeatureCollection),
       },
     });
@@ -17,5 +41,49 @@ describe("@/components/EuropeanaMap.vue", () => {
     const map = wrapper.get("#europeana-map-map");
 
     expect(map.isVisible()).toBe(true);
+  });
+
+  describe("data", () => {
+    describe("when supplied `json` in props", () => {
+      const props = {
+        json: JSON.stringify(fixtures.onePointFeatureCollection),
+      };
+
+      it("uses parsed JSON for data value", () => {
+        const wrapper = factory({ props });
+
+        const data = wrapper.vm.data;
+
+        expect(data).toEqual(fixtures.onePointFeatureCollection);
+      });
+    });
+
+    describe("when supplied `url` in props", () => {
+      const props = { url: fixtures.url };
+
+      it("fetches GeoJSON from URL", async () => {
+        const wrapper = factory({ props });
+
+        await new Promise(process.nextTick);
+        const data = wrapper.vm.data;
+
+        expect(data).toEqual(fixtures.twoPointsFeatureCollection);
+      });
+    });
+
+    describe("when supplied neither in props", () => {
+      const props = {};
+
+      it("throws an error", () => {
+        let error;
+        try {
+          factory({ props });
+        } catch (e) {
+          error = e;
+        }
+
+        expect(error.message).toBe("No data JSON or URL supplied in props.");
+      });
+    });
   });
 });
--- packages/components/map/src/components/EuropeanaMap.vue
+++ packages/components/map/src/components/EuropeanaMap.vue
@@ -1,16 +1,31 @@
 <script setup>
+import { ref } from "vue";
+import { useFetch } from "@vueuse/core";
 import { configProps } from "@/config.js";
-import { useMapData } from "@/composables/mapData.js";
 import { useOpenLayersMap } from "@/composables/openLayersMap.js";
 import { useOpenLayersPointClusters } from "@/composables/openLayersPointClusters.js";
 import pointIconSrc from "@/assets/img/ic_location.svg";
 import "ol/ol.css";
 
 const props = defineProps(configProps);
+
+const data = ref(null);
+
+if (props.json) {
+  data.value = JSON.parse(props.json);
+} else if (props.url) {
+  useFetch(props.url)
+    .json()
+    .then((fetched) => {
+      data.value = fetched.data.value;
+    });
+} else {
+  throw new Error("No data JSON or URL supplied in props.");
+}
+
 const target = "europeana-map-map";
 
-const { data, centre } = useMapData({ json: props.json, url: props.url });
-const { map } = useOpenLayersMap({ centre, target });
+const { map } = useOpenLayersMap({ target });
 useOpenLayersPointClusters({ data, map, pointIconSrc });
 </script>
 
