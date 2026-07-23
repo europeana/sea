2126033ea06bb67ab98d748573d167b08be0c653
Richard Doe <richard.doe@rwdit.net>
2026-07-23 14:47:25 +0100

    feat(map-styles): add map-styles package (#354)
    * feat(map-styles): add map-styles package
    
    * chore(map): switch README contents
    
    * docs(map-styles): extend README
    
    * chore(map-styles): rename output files
    
    to follow pkg name
    
    * chore(map-styles): mv tasks to src file

---
+++ packages/style/map-styles/README.md
@@ -0,0 +1,20 @@
+# Europeana Map styles
+
+Custom styles for `@europeana/map`.
+
+Includes two custom styles:
+
+- Protomaps, which builds to 25 European languages
+- Versatiles, which builds to 9 European languages
+
+Each style has a base StyleJSON file and a script responsible for generating
+multiple versions of the same, e.g. for supported locale, to be included in
+the published package.
+
+Build with `pnpm  build`.
+
+Once published, use a CDN to load the required version of the style.
+
+## License
+
+Licensed under the [EUPL v1.2](../../../LICENSE.md).
---
+++ packages/style/map-styles/src/versatiles/index.js
@@ -0,0 +1,58 @@
+import baseStyle from "./style.json" with { type: "json" };
+
+// European languages supported by Europeana projects, where also supported by Versatiles
+export const LOCALES = new Set([
+  "de",
+  "el",
+  "en",
+  "es",
+  "fr",
+  "it",
+  "nl",
+  "pl",
+  "pt",
+]);
+
+export const localise = (locale) => {
+  if (!LOCALES.has(locale)) {
+    throw new Error(`Unsupported locale ${locale}`);
+  }
+
+  const style = {
+    ...baseStyle,
+    layers: baseStyle.layers.map((layer) => {
+      if (layer.layout?.["text-field"]) {
+        const textField = layer.layout?.["text-field"];
+        if (
+          textField.length === 2 &&
+          textField[0] === "get" &&
+          textField[1] === "name"
+        ) {
+          return {
+            ...layer,
+            layout: {
+              ...layer.layout,
+              "text-field": [
+                "coalesce",
+                ["get", `name_${locale}`],
+                ["get", "name"],
+              ],
+            },
+          };
+        }
+      }
+      return layer;
+    }),
+  };
+
+  return style;
+};
+
+export function* build() {
+  for (const locale of LOCALES.values()) {
+    yield {
+      file: `europeana-map-styles.versatiles.${locale}.json`,
+      data: JSON.stringify(localise(locale)),
+    };
+  }
+}
