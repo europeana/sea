3dc51c00f52fad0dbbfb1d019bbe2b4fad08e996
Lutz Biedinger <1177719+lbiedinger@users.noreply.github.com>
2026-09-17 14:35:08 +0200

    feat: create contentful apps nuxt app (#378)
    * feat: (wip) new nxut app for contentful apps
    
    * chore: fix package lock file
    
    * chore: rename new contetnful package to ctf-apps

---
+++ packages/apps/ctf-apps/layouts/default.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <div>
+    <Html>
+      <Head> </Head>
+      <Body>
+        <div>
+          <main id="main" role="main">
+            <slot />
+          </main>
+        </div>
+      </Body>
+    </Html>
+  </div>
+</template>
