1d7310c2ce4930b5698b220960821320d2c6f402
Richard Doe <richard.doe@rwdit.net>
2026-09-21 11:23:33 +0100

    feat(ds4ch): load directory listing from URL

--- packages/apps/ds4ch/components/Generic/DirectoryBrowser.vue
+++ packages/apps/ds4ch/components/Generic/DirectoryBrowser.vue
@@ -1,7 +1,7 @@
 <script setup>
-defineProps({
-  directories: {
-    type: Array,
+const props = defineProps({
+  url: {
+    type: String,
     required: true,
   },
   level: {
@@ -9,57 +9,82 @@ defineProps({
     default: 0,
   },
 });
+
+const opened = ref([]);
+
+const handleClickAccordionButton = (item) => {
+  opened.value.push(item.url);
+};
+
+const isOpen = (item) => opened.value.includes(item.url);
+
+const itemURL = (item) => {
+  let url = `${props.url}/${item.name}`;
+  if (item.type === "directory") {
+    url = `${url}/`;
+  }
+  url = url.replace(/\/\//g, "/");
+  return url;
+};
+
+const { data } = useAsyncData(`DirectoryBrowser:${props.url}`, () =>
+  $fetch(props.url),
+);
+
+const items = computed(() =>
+  data.value.map((item) => ({
+    ...item,
+    url: itemURL(item),
+  })),
+);
 </script>
 
 <template>
   <div :id="`directory-browser-${level}`" class="accordion accordion-flush">
     <div
-      v-for="(directory, index) in directories"
-      :key="directory.name + index"
+      v-for="(item, index) in items"
+      :key="item.name + index"
       class="accordion-item"
     >
-      <div class="accordion-header">
-        <button
-          class="accordion-button collapsed"
-          type="button"
-          data-bs-toggle="collapse"
-          :data-bs-target="`#collapse-${index}-${level}`"
-          aria-expanded="false"
-          :aria-controls="`collapse-${index}-${level}`"
-        >
-          {{ directory.name }}
-        </button>
-      </div>
-      <div
-        :id="`collapse-${index}-${level}`"
-        class="accordion-collapse collapse"
-        :data-bs-parent="`directory-browser-${level}`"
-      >
-        <div class="accordion-body">
-          <template
-            v-for="(item, subIndex) in directory.items"
-            :key="item.name + subIndex"
+      <template v-if="item.type === 'directory'">
+        <div class="accordion-header">
+          <button
+            class="accordion-button collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            :data-bs-target="`#collapse-${index}-${level}`"
+            aria-expanded="false"
+            :aria-controls="`collapse-${index}-${level}`"
+            @click="handleClickAccordionButton(item)"
           >
+            {{ item.name }}
+          </button>
+        </div>
+        <div
+          :id="`collapse-${index}-${level}`"
+          class="accordion-collapse collapse"
+          :data-bs-parent="`directory-browser-${level}`"
+        >
+          <div class="accordion-body">
             <DirectoryBrowser
-              v-if="item.type === 'directory'"
-              :directories="[item]"
+              v-if="isOpen(item)"
+              :url="item.url"
               :level="level + 1"
             />
-            <!-- TODO: add actual file URL -->
-            <GenericSmartLink
-              v-if="item.type === 'file'"
-              destination="/"
-              class="file-link text-decoration-none d-flex align-items-center p-3"
-            >
-              <span class="icon-file me-2" />
-              <span class="link-text">{{ item.name }}</span>
-              <span
-                class="icon-ic-download d-flex align-items-center justify-content-center ms-auto"
-              />
-            </GenericSmartLink>
-          </template>
+          </div>
         </div>
-      </div>
+      </template>
+      <GenericSmartLink
+        v-else-if="item.type === 'file'"
+        :destination="item.url"
+        class="file-link text-decoration-none d-flex align-items-center p-3"
+      >
+        <span class="icon-file me-2" />
+        <span class="link-text">{{ item.name }}</span>
+        <span
+          class="icon-ic-download d-flex align-items-center justify-content-center ms-auto"
+        />
+      </GenericSmartLink>
     </div>
   </div>
 </template>
---
+++ packages/apps/ds4ch/pages/dir.vue
@@ -0,0 +1,9 @@
+<script setup>
+// NOTE: this is a temporary page for previewing live directory listing data
+</script>
+
+<template>
+  <GenericDirectoryBrowser
+    url="https://pro-beta.europeana.eu/files/Europeana_Professional/"
+  />
+</template>
