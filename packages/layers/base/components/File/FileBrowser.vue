2713815753bdee131f1396d4793e828ade5b6ff1
Leonie Peters <leoniepeters@gmail.com>
2026-09-23 13:46:40 +0200

    style(base): remove ds4ch scss dependency

--- packages/layers/base/components/File/FileBrowser.vue
+++ packages/layers/base/components/File/FileBrowser.vue
@@ -99,7 +99,6 @@ const items = computed(
 
 <style lang="scss" scoped>
 @import "@europeana/style/scss/variables";
-@import "assets/scss/variables";
 @import "@europeana/style/scss/icon-font";
 
 .accordion {
@@ -160,12 +159,12 @@ const items = computed(
   background-color: $black;
   color: $white;
   border-radius: 50%;
-  font-size: $font-size-18;
+  font-size: 1.125rem;
 
   @media (min-width: $bp-4k) {
     height: 3rem;
     width: 3rem;
-    font-size: $font-size-36;
+    font-size: 2.25rem;
   }
 }
 </style>
