92734ac08ba4c3bfe3159e430e2f29b7925b0ec8
Leonie Peters <leoniepeters@gmail.com>
2026-09-24 10:18:52 +0200

    chore(ctf-apps): bootstrap plugin at base layer

--- packages/apps/ctf-apps/layouts/default.vue
+++ packages/apps/ctf-apps/layouts/default.vue
@@ -1,17 +1,5 @@
 <script setup>
 useHead({
-  link: [
-    // Version is locked, re-implement version lookup via pkg-versions.js.
-    {
-      rel: "preload",
-      as: "style",
-      href: `https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css`,
-    },
-    {
-      rel: "stylesheet",
-      href: `https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css`,
-    },
-  ],
   meta: [{ hid: "robots", name: "robots", content: "noindex" }],
 });
 </script>
