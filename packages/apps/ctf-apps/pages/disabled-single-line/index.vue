1653499afa981ca3b2d1ad47db615890ed6295b2
Leonie Peters <45720401+LeoniePeters@users.noreply.github.com>
2026-09-25 10:04:53 +0200

    feat(ctf-apps): add clear button to document-linker (#387)
    * feat(ctf-apps): add clear button to document-linker; clean up styles
    
    * test(ctf-apps): correct test text
    
    ---------
    
    Co-authored-by: Richard Doe <richard.doe@rwdit.net>

--- packages/apps/ctf-apps/pages/disabled-single-line/index.vue
+++ packages/apps/ctf-apps/pages/disabled-single-line/index.vue
@@ -45,18 +45,3 @@ onMounted(() => {
     </form>
   </div>
 </template>
-
-<style lang="scss" scoped>
-.contentful {
-  font-size: 11px;
-
-  input:disabled {
-    // thse styles need to be moved to somewhere where they can be shared among contentful applications
-    background-color: #e9ecef;
-    padding: 0.375rem 0.75rem;
-    font-size: 1rem;
-    border-radius: 0.25rem;
-    border: 0;
-  }
-}
-</style>
